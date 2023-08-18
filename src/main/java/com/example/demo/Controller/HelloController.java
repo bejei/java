f7377ed8503fc0722d6d6b05666fdb8b9e6c2cb4
best-init <thebestofme676@gmail.com>
2023-08-18 22:39:45 +0800

    aaaaaaaa

--- src/main/java/com/example/demo/Controller/HelloController.java
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -1,4 +1,12 @@
 package com.example.demo.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello";
+    }
 }
